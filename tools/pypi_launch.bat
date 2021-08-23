763aa92a230bf30852cb43994197a0f0357243a2
Yohann Boniface <edhyjox@gmail.com>
2021-08-23 23:06:05 +0200

    🐛 add cd ..

--- tools/pypi_launch.bat
+++ tools/pypi_launch.bat
@@ -1,3 +1,4 @@
+cd ..
 venv\Scripts\python.exe -m setup sdist bdist_wheel
 venv\Scripts\python.exe -m twine check dist/*
 venv\Scripts\python.exe -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u %1 -p %2 --skip-existing

34d95d1124c3e58c8a4289111a3eea67558c2f16
Yohann Boniface <edhyjox@gmail.com>
2021-08-29 18:11:32 +0200

    Delete pypi_launch.bat

--- tools/pypi_launch.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-venv\Scripts\python.exe -m setup sdist bdist_wheel
-venv\Scripts\python.exe -m twine check dist/*
-venv\Scripts\python.exe -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/* -u %1 -p %2 --skip-existing
